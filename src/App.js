736980eb4a5d8e41cfb6fe2b72f553b2d8829858
Hari Bhatta <haree5bhatta@gmail.com>
2023-03-19 19:32:30 +1100

    portfoliov2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import "./App.css";
-import { AboutMe } from "./components/AboutMe";
-import { Contact } from "./components/Contact";
-import { Hero } from "./components/Hero";
-import { Projects } from "./components/Projects";
-import { Skills } from "./components/Skills";
-import "./style.css";
-import { Routes, Route } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import React from "react";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="skills" element={<Skills />} />
-        <Route path="project" element={<Projects />} />
-        <Route path="about-me" element={<AboutMe />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </>
+    <div className="">
+      <Button>
+        <i class="fa-solid fa-face-smile"></i>
+      </Button>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,5 @@
 import React from "react";
 
 export const Header = () => {
-  return (
-    <header className="primary-header-flex ">
-      <div className="logo flex">
-        <div>
-          <h1>Hari Bhatta</h1>
-        </div>
-
-        <div>
-          <h1 class="line">
-            {"<"}
-            Front-end Developer
-            {"/>"}
-          </h1>
-        </div>
-      </div>
-
-      <div className="right">
-        <input type="" />
-      </div>
-    </header>
-  );
+  return <div>Header</div>;
 };
